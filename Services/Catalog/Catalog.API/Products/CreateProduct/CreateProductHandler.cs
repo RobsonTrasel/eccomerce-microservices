8dcdac230a2514445c16f2204b539123b868cc6d
RobsonTrasel <robson.trasel@gmail.com>
2024-06-20 17:25:30 -0400

    feat(product): criado e implementado MediatR no CreateProductCommandHandler

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+
+    
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(String Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
