84e3598cb8c5e5962cde481d594b23d9800323f2
RobsonTrasel <robson.trasel@gmail.com>
2024-06-22 16:15:03 -0400

    feat(product): criado handler para pesquisar o produto pelo id

--- Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdHandler
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+
+        }
     }
 }
