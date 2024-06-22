e9f4e05fc6e1c4e598c88fec5c8a81b55278d8fc
RobsonTrasel <robson.trasel@gmail.com>
2024-06-22 16:15:12 -0400

    feat(product): criado endpoint para pesquisar o produto pelo id

--- Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdEndpoint
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(Id));
+
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id")
+            ;
+        }
     }
 }
